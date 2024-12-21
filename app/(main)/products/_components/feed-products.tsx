db865d611ac1a21bd701e281682750c6a12c0223
DarawanOmar <darait721@gmail.com>
2024-12-22 00:43:50 +0300

    push

--- app/(main)/products/_components/feed-products.tsx
+++ app/(main)/products/_components/feed-products.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import CardProduct from "./card-product";
 import { getAllProducts } from "../_action";
 import EmptyImage from "@/components/reuseable/empty-image";
+import { Product } from "../_type";
 
 async function FeedProducts() {
-  const allProducts = await getAllProducts();
+  const allProducts: Product[] = await getAllProducts();
   if (allProducts.length === 0) return <EmptyImage />;
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10">
